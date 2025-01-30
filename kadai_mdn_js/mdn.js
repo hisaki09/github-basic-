094cc1a50ddf8ad19d05fa0b9529a5df4ee608b1
hisaki09 <f.hisaki.0925@gmail.com>
2025-01-30 13:45:36 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,8 @@
-const say = () => {
-console.log(new Date(8.64e15).toString()); // "Sat Sep 13 275760 00:00:00 GMT+0000 (Coordinated Universal Time)"
-console.log(new Date(8.64e15 + 1).toString()); // "Invalid Date"
-}
-say();
+const date = new Date();
+const [year,month,day] = [
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+];
+
+console.log(date);
